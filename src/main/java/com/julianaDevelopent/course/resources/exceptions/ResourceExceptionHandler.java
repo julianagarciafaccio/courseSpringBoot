9e3cfed58e544829bf98365b82c68e8047eb6260
Juliana Garcia <julianafaccio6@gmail.com>
2024-03-23 09:53:23 -0300

    exception handler

--- src/main/java/com/julianaDevelopent/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/julianaDevelopent/course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.julianaDevelopent.course.resources.exceptions;
 
+import com.julianaDevelopent.course.services.exceptions.DatabaseException;
 import com.julianaDevelopent.course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -18,5 +19,13 @@ public class ResourceExceptionHandler extends RuntimeException{
         StandardError err = new StandardError(Instant.now(), status.value(), error , e.getMessage(),request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
+    @ExceptionHandler(DatabaseException.class) // essa notaçao serve para indicar q esse metodo e para pegar o erro e interceptar e tratar as exceçoes q estao aqui dentro
+    public ResponseEntity<StandardError> database(ResourceNotFoundException e , HttpServletRequest request){
+        String error = "Database Error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error , e.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
 
 }
